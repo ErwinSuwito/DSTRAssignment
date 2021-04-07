f7e3343c5a042129dd058f59f0267c4f296c9f37
Erwin Suwitoandojo <30384397+ErwinSuwito@users.noreply.github.com>
2021-04-07 12:01:11 +0700

    Added code to show main menu

--- DSTRAssignment/DSTRAssignment.cpp
+++ DSTRAssignment/DSTRAssignment.cpp
@@ -2,12 +2,55 @@
 #include <iostream>
 using namespace std;
 
-void displayMenu()
+void showMainMenu()
 {
-    
+    cout << endl << endl << "*** Pet inventory System ***" << endl;
+    cout << "To perform an action, enter:" << endl;
+    cout << "1 to add new pet" << endl;
+    cout << "2 to browse pets" << endl;
+    cout << "3 to search pets" << endl;
+    cout << "4 to Make purchase" << endl;
+    cout << "5 to view transaction history" << endl << endl;
+
+    cout << "0 to exit" << endl;
+    cout << "Selection: ";
 }
 
 int main()
 {
-    
+    int input;
+    showMainMenu();
+    cin >> input;
+
+    while (input != 0)
+    {
+        switch (input)
+        {
+            case 1: 
+                cout << "1 selected" << endl;
+                break;
+
+            case 2:
+                cout << "2 selected" << endl;
+                break;
+
+            case 3:
+                cout << "3 selected" << endl;
+                break;
+
+            case 4:
+                cout << "4 selected" << endl;
+                break;
+
+            case 5:
+                cout << "5 selected" << endl;
+                break;
+
+            default:
+                cout << "Invalid selection. " << endl;
+        }
+
+        showMainMenu();
+        cin >> input;
+    }
 }
